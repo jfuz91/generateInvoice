a79451fc4eb05002aea2a716e89564db5e54c572
jfuz91 <felipeuz.07@gmail.com>
2025-08-08 23:40:41 -0500

    Add function that returns fixed service charge

--- scrip.js
+++ scrip.js
@@ -2,7 +2,6 @@ function obtenerConsumoMensual(){
     let consumo = parseFloat(prompt("Ingrese su consumo mensual en kWh:"));
     return consumo;
 }
-
 let consumoUsuario = obtenerConsumoMensual();
 console.log("Consumo ingresado:", consumoUsuario);
 
@@ -10,6 +9,13 @@ function obtenerTarifaBase(){
     let tarifa = Number(prompt("Ingrese la tarifa base por kWh:"));
     return tarifa;
 }
-
 let tarifa = obtenerTarifaBase();
 console.log("tarifa base:", tarifa);
+
+const cargoFijo = function(){
+    let cargoFijo = Number(prompt("Ingrese el valor cargo fijo por servicio: "))
+    return cargoFijo
+}
+let cargo = cargoFijo()
+
+
